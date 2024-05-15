434e7dda3029109d6c8fb0f95694ad66b904e6b2
utsav-patidar <patelutsav22082003@gmail.com>
2024-05-15 10:06:49 +0530

    add pwa for mobile app

--- app/layout.js
+++ app/layout.js
@@ -9,9 +9,27 @@ import "./css/globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of utsav patel - Software Developer",
+  title: "Portfolio of Utsav Patel - Software Developer",
   description:
     "This is the portfolio of utsav patel. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+  manifest: "/manifest.json",
+  keywords: [
+    "nextjs",
+    "nextjs14",
+    "next14",
+    "pwa",
+    "next-pwa",
+    "Utsav",
+    "Hiring",
+    "Mern Stack",
+    "Portfolio",
+  ],
+  authors: [
+    {
+      name: "Utsav Patel",
+      url: "https://github.com/utsavPatidar9316/",
+    },
+  ],
 };
 
 export default function RootLayout({ children }) {
